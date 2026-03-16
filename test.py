2a7de4571d9be574b6576aaacacc7e1c836c5e10
Andrea <ggandream24@gmail.com>
2026-03-15 20:32:23 -0600

    simulate failure

--- test.py
+++ test.py
@@ -16,16 +16,6 @@ def test_get_accounts_returns_all_accounts() -> None:
     assert data[0]["accountNumber"] == "1001"
 
 
-def test_get_account_by_number_returns_account() -> None:
-    response = client.get("/accounts/1002")
-
-    assert response.status_code == 200
-    data = response.json()
-    assert data["accountNumber"] == "1002"
-    assert data["firstName"] == "Carlos"
-    assert data["lastName"] == "Ramirez"
-
-
 def test_get_account_by_number_not_found() -> None:
     response = client.get("/accounts/9999")
 
