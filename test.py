a71d0bd72d6957407447b2018cf1ba8ff42bd1f8
Andrea <ggandream24@gmail.com>
2026-03-15 20:33:43 -0600

    simulate failure

--- test.py
+++ test.py
@@ -6,15 +6,6 @@ from main import app
 client = TestClient(app)
 
 
-def test_get_accounts_returns_all_accounts() -> None:
-    response = client.get("/accounts")
-
-    assert response.status_code == 200
-    data = response.json()
-    assert isinstance(data, list)
-    assert len(data) == 3
-    assert data[0]["accountNumber"] == "1001"
-
 
 def test_get_account_by_number_not_found() -> None:
     response = client.get("/accounts/9999")
